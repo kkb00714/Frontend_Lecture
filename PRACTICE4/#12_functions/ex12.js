c391e9f4692b558946c07167658d1dd2ea448d4d
kkb <0furiousblade0@gmail.com>
2023-12-27 18:45:20 +0900

    [#50 UPDATE] JavaScript - Functions (1)

---
+++ PRACTICE4/#12_functions/ex12.js
@@ -0,0 +1,31 @@
+/*
+    함수(functions)
+
+    - 작업의 묶음
+    - 코드를 반복해서 작성하는 비효율을 줄임
+*/
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+// console.log(multiply(10, 2));
+
+// var result = multiply(432, 21);
+// console.log(result);
+
+
+
+// 함수 표현식
+var multiply = function calc (a, b) {
+    return a * b;
+};
+console.log(multiply(10, 5));
+// console.log(calc(10, 5)); // 에러
+
+
+
+
+
+
+
+
