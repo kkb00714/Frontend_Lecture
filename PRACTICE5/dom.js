9c95255320d23ab64d6ec56a9a77ff05f44e6ec3
kkb <0furiousblade0@gmail.com>
2023-12-27 23:26:06 +0900

    [#55 UPDATE] JavaScript on the Next Level (3)

--- PRACTICE5/dom.js
+++ PRACTICE5/dom.js
@@ -1,13 +1,33 @@
-var el = document.getElementById("brand-title");
+// var el = document.getElementById("brand-title");
 
-console.log(el);
+// console.log(el);
 
-console.log(el.innerHTML);
-console.log(el.innerText);
+// console.log(el.innerHTML);
+// console.log(el.innerText);
 
-el.innerText = "안녕하세요 :)"
+// el.innerText = "안녕하세요 :)"
 
-var el = document.getElementsByClassName('sub-title');
+// var el = document.getElementsByClassName('sub-title');
 
-console.log(el);
+// console.log(el);
+
+
+/* 
+    이벤트
+
+    - 폼 이벤트 
+        => 사용자가 제출, 초기화 하는 이벤트
+
+    - 마우스 이벤트
+        => 클릭, 더블클릭, 마우스 이동
+
+    - 키보드 이벤드
+        => keydown, keypress, keyup 이벤트 
+
+    ex) on + 이벤트 타입
+*/
+
+
+
+
 
