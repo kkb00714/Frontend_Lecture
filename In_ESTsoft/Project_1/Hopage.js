36cb13f4b91640c656dc49f6f16dd3711256a7f6
kkb <0furiousblade0@gmail.com>
2024-08-22 10:53:19 +0900

    [#180] Front-Project 16 - Top button 생성

--- In_ESTsoft/Project_1/Hopage.js
+++ In_ESTsoft/Project_1/Hopage.js
@@ -10,9 +10,28 @@ function closeModal() {
     console.log("JavaScript is working");
 }
 
-// 모달 외부 클릭 시 모달 닫기
-window.onclick = function(event) {
-    if (event.target == document.getElementById("modal")) {
-        closeModal();
+// 모달 외부 클릭 시 모달 닫기 (임시)
+window.onclick = function(event) { 
+    if (event.target == document.getElementById("modal")) { 
+        closeModal(); 
+    } 
+} 
+
+document.querySelector('.upbutton-center').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', function() {
+    const upButton = document.getElementById('upbutton');
+    const footer = document.querySelector('footer');
+    
+    const footerTop = footer.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (window.scrollY > 100) {
+        // 특정 픽셀 이상 스크롤했을 때
+        upButton.classList.add('show-upbutton');
+    } else {
+        upButton.classList.remove('show-upbutton');
     }
-}
+});
