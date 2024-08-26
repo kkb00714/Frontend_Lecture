7ec1a9a710cfa76a6d8ca70631d00c23ee22175d
kkb <0furiousblade0@gmail.com>
2024-08-26 18:48:02 +0900

    [#186] Front-Project 22 - 모바일 First-Page CSS

--- In_ESTsoft/Project_1/Hopage.js
+++ In_ESTsoft/Project_1/Hopage.js
@@ -43,4 +43,7 @@ window.addEventListener('DOMContentLoaded', function() {
 function toggleMenu() {
     const menu = document.getElementById('mobile-menu');
     menu.classList.toggle('open');
+
+    const icon = document.getElementById('menu-icon');
+    icon.classList.toggle('close-icon');
 }
