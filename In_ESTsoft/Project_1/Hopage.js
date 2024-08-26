398bd3a61a0128a08b3303a849c030454844c1a8
kkb <0furiousblade0@gmail.com>
2024-08-26 11:04:25 +0900

    [#181] Front-Project 17 - Footer CSS 수정

--- In_ESTsoft/Project_1/Hopage.js
+++ In_ESTsoft/Project_1/Hopage.js
@@ -1,5 +1,4 @@
-console.log("JavaScript is working");
-
+// 모달
 function openModal() {
     document.getElementById("modal").style.display = "block";
     console.log("JavaScript is working");
@@ -10,28 +9,24 @@ function closeModal() {
     console.log("JavaScript is working");
 }
 
-// 모달 외부 클릭 시 모달 닫기 (임시)
-window.onclick = function(event) { 
-    if (event.target == document.getElementById("modal")) { 
-        closeModal(); 
-    } 
-} 
+// 스크롤 이벤트 처리
+window.addEventListener('DOMContentLoaded', function() {
+    const upButton = document.querySelector('.upbutton');
+    const lastPicture = document.querySelector('.last-picture');
 
-document.querySelector('.upbutton-center').addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+    // 스크롤이 100 이상일 때 버튼 노출
+    window.addEventListener('scroll', function() {
+        const lastPicturePosition = lastPicture.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
 
-window.addEventListener('scroll', function() {
-    const upButton = document.getElementById('upbutton');
-    const footer = document.querySelector('footer');
+        if (window.scrollY > 100) {
+            upButton.classList.add('show-upbutton');
+        } else {
+            upButton.classList.remove('show-upbutton');
+        }
+    });
     
-    const footerTop = footer.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    if (window.scrollY > 100) {
-        // 특정 픽셀 이상 스크롤했을 때
-        upButton.classList.add('show-upbutton');
-    } else {
-        upButton.classList.remove('show-upbutton');
-    }
+    upButton.addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
 });
